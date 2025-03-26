d1027a33156bbc6dbc617d0e9260196b18600802
mayainni <pashakuz1022@gmail.com>
2025-03-26 17:42:35 +0300

    Create main

---
+++ LR3.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void InputNatNumber(){
+    int X;
+    while (true) {
+        cout << "Введите натуральное число X: ";
+        cin >> X;
+
+        
+        if (cin.fail() || X <= 0) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Ошибка! Введите корректное натуральное число.\n";
+        } else {
+            return X;
+        }
+    }
+}
+
+bool TrueFerstAndEndNumber(int X){
+    string strX = to_string(X);
+    return strX.front() == strX.back();
+}
+
+bool TrueNecheEndNumber(int X){
+    int lastDigit = X % 10;
+    return lastDigit % 2 != 0;
+}
+
+struct MenuItem {
+    string title;
+    function<void()> action;
+};
+
+int main() {
+    std::map<int, MenuItem> menu = {
+        {1, {"Task 1", }},
+        {2, {"Task 2", TrueFerstAndEndNumber}},
+        {3, {"Task 3", TrueNecheEndNumber}}
+    }
+        
+int choice = 0;
+while (true) {
+    std::cout << "Menu: " << std::endl;
+    for (const auto& item : menu) {
+        std::cout << " " << item.first << ". " << item.second.title << std::endl;
+    }
+    std::cout << "0. Exit" << std::endl;
+
+    EnterNumberInt(choice, "Input number: ")();
+    if (choice == 0) {
+        std::cout << "© 2025 FirstName LastName" << std::endl;
+        break;
+    }
+    std::cout << std::endl;
+
+    if (menu.find(choice) != menu.end()) {
+        menu[choice].action();
+    }
+    else {
+        std::cout << "Oshibka." << std::endl;
+    }
+
+    std::cout << std::endl << std::endl;
+    }
+    return 0;
+}
