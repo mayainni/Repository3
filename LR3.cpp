84762bca317cd9708218e39668eca4ff8454d8f1
mayainni <pashakuz1022@gmail.com>
2025-03-26 17:50:23 +0300

    create task 1

--- LR3.cpp
+++ LR3.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <functional>
 
 using namespace std;
+
+function<void()> EnterNumberInt(int& varLink, string label){
+    return [&varLink, label](){
+        string raw_input;
+        cout << label << " = ";
+        getline(cin,raw_input);
+        while (!UserInputInt(raw_input))
+        {
+            cout << label << " = ";
+            getline(cin,raw_input);
+        }
+        varLink = stoi(raw_input);
+    };
+}
 
 void InputNatNumber(){
     int X;
@@ -37,34 +53,34 @@ struct MenuItem {
 
 int main() {
     std::map<int, MenuItem> menu = {
-        {1, {"Task 1", }},
+        {1, {"Task 1", InputNatNumber}},
         {2, {"Task 2", TrueFerstAndEndNumber}},
         {3, {"Task 3", TrueNecheEndNumber}}
     }
         
 int choice = 0;
 while (true) {
-    std::cout << "Menu: " << std::endl;
+    cout << "Menu: " << endl;
     for (const auto& item : menu) {
-        std::cout << " " << item.first << ". " << item.second.title << std::endl;
+        cout << " " << item.first << ". " << item.second.title << endl;
     }
-    std::cout << "0. Exit" << std::endl;
+    cout << "0. Exit" << endl;
 
     EnterNumberInt(choice, "Input number: ")();
     if (choice == 0) {
-        std::cout << "© 2025 FirstName LastName" << std::endl;
+        cout << "© 2025 FirstName LastName" << endl;
         break;
     }
-    std::cout << std::endl;
+    cout << endl;
 
     if (menu.find(choice) != menu.end()) {
         menu[choice].action();
     }
     else {
-        std::cout << "Oshibka." << std::endl;
+        cout << "Oshibka." << endl;
     }
 
-    std::cout << std::endl << std::endl;
+    cout << endl << endl;
     }
     return 0;
 }
